340a2644e2b92e0a795c9d955cb486cb82ec7bf9
Pratik-Ghost <134989001+Pratik-Ghost@users.noreply.github.com>
2023-06-07 19:12:56 +0545

    Update About.php

--- About.php
+++ About.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        .about-me {
+    <title>About</title>
+        <style>
+    body{
+                background-image:url(https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg);
+                 background-repeat:no-repeat;
+                 background-size:cover;
+    }
+    .about-me {
     display: flex;
     justify-content: center;
     align-items: center;
