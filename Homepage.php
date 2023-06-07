4b337540594d92a531f741931a12954689281df2
Pratik-Ghost <134989001+Pratik-Ghost@users.noreply.github.com>
2023-06-07 19:16:22 +0545

    Add files via upload

---
+++ Homepage.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Simple Quiz App HomePage</title>
+    <link rel="stylesheet" href="Home.css">
+</head>
+
+<body>
+    <header class="header">
+        <a href="Homepage.html" class="logo">Quiz App</a>
+        <nav class="nav-items">
+            <a href="About.php">About</a>
+            <a href="Login.php">Login</a>
+            <a href="Register.php">Register</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+    </header>
+    <main>
+        <div class="intro">
+            <h1>Project Of Web Page Development</h1>
+            <p>- Quiz the Best, Win the Rest!</p>
+            <p>-Let the Quiz Begin!</p>
+
+            <button><a href="Login.php">Login</a></button>
+        </div>
+
+        <div class="about-me">
+            <div class="about-me-text">
+                <h2>Why us?</h2><br>
+                <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed, officia. <br> Asperiores impedit, adipisci accusamus, optio doloremque expedita ex id laboriosam ea magni, <br> totam nihil nisi facilis quo placeat qui aperiam!   <br>
+                    <br>
+            </div>
+            <img src="https://source.unsplash.com/random/?city,night" alt="Random Pic">
+        </div>
+    </main>
+    <footer class="footer">
+        <div class="copy">&copy; 2023 Quiz App</div>
+        <div class="bottom-links">
+            <div class="links">
+                <span>More Info</span>
+                <a href="About.php">About</a>
+                <a href="contact.php">Contact</a>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
