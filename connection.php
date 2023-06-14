a835be4c0e804bc0c00f574be2a922b8017fe46c
Pratik-Ghost <134989001+Pratik-Ghost@users.noreply.github.com>
2023-06-14 08:34:34 +0545

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
- <?php 
- $host="localhost";
- $port="443";
- $username="root";
- $password="";
- $database="user";
- $con = mysqli_connect("localhost","root","","loginpage") or die("Couldn't connect");
-?>
