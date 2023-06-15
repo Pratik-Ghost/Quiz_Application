8360e64a1603b06363b53ed889fa15df38c58721
Pravinn-Xtha <135080817+Pravin-Stha@users.noreply.github.com>
2023-06-15 06:32:02 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,174 @@
+<html>
+<head>
+<title>Project</title>
+<link  rel="stylesheet" href="css/bootstrap.min.css"/>
+ <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
+ <link rel="stylesheet" href="css/main.css">
+ <link  rel="stylesheet" href="css/font.css">
+ <script src="js/jquery.js" type="text/javascript"></script>
+ <link rel="stylesheet" type="text/css" href="index.css">
+  <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+</head>
+<body>
+<div class="header">
+<div class="row">
+<div class="col-lg-6">
+<span class="logo">Kuiz App</span></div>
+<div class="col-md-2 col-md-offset-4">
+<a href="#" class="pull-right btn sub1" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Admin</b></span></a></div>
+<!-- Admin Login -->
+<div class="modal fade" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content title1">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title title1"><span style="color:orange">Log In</span></h4>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal" action="admin.php?q=index.php" method="POST">
+<fieldset>
+<div class="form-group"> <h3 style="text-align:center;">Admin Login</h3><br>
+  <label class="col-md-3 control-label" for="email"></label>  
+  <div class="col-md-6">
+  <input id="email" name="email" placeholder="Admin username" class="form-control input-md" type="email" required>
+    
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-3 control-label" for="password"></label>
+  <div class="col-md-6">
+    <input id="password" name="password" placeholder="Admin Password" class="form-control input-md" type="password" required>
+  </div>
+</div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Log in</button>
+		</fieldset>
+</form>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+<main>
+<div class="bg1">
+<div class="row">
+<div class="col-md-7"></div>
+<div class="col-md-4 panel">
+ 
+
+  <form class="form-horizontal" name="form" action="sign.php?q=account.php" onSubmit="return validateForm()" method="POST">
+<fieldset> <h1 style="text-align:center;">signin</h1><br>
+<div class="form-group">
+  <label class="col-md-12 control-label" for="name"></label>  
+  <div class="col-md-12">
+  <input id="name" name="name" placeholder="Enter your name" class="form-control input-md" type="text" required>
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-12 control-label title1" for="email"></label>
+  <div class="col-md-12">
+    <input id="email" name="email" placeholder="Enter your email" class="form-control input-md" type="email" required>
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-12 control-label" for="password"></label>
+  <div class="col-md-12">
+    <input id="password" name="password" placeholder="Enter your password" class="form-control input-md" type="password" required>
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-12control-label" for="cpassword"></label>
+  <div class="col-md-12">
+    <input id="cpassword" name="cpassword" placeholder="Conform Password" class="form-control input-md" type="password" required>
+    
+  </div>
+</div>
+<?php if(@$_GET['q7'])
+{ echo'<p style="color:red;font-size:15px;">'.@$_GET['q7'];}?>
+<div class="form-group">
+  <label class="col-md-12 control-label" for=""></label>
+  <div class="col-md-12"> 
+    <input  type="submit" class="sub" value="sign in" class="btn btn-primary"/>
+  </div>
+</div>
+</fieldset>
+</form>
+</div>
+</div></div>
+</div>
+</main>
+<!--Footer start-->
+<div class="row footer">
+<div class="col-md-3 box">
+<a href="About.php" target="_blank">About us</a>
+</div>
+<div class="col-md-3 box">
+<a href="#" data-toggle="modal" data-target="#login">Admin Login</a></div>
+<div class="col-md-3 box">
+<a href="#" data-toggle="modal" data-target="#developers">Developers</a>
+</div>
+<div class="col-md-3 box">
+<a href="feedback.php" target="_blank">Feedback</a></div></div>
+
+<div class="modal fade title1" id="developers">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" style="font-family:'typo' "><span style="color:orange">Developers</span></h4>
+      </div>
+	  
+      <div class="modal-body">
+        <p>
+		<div class="row">
+		<div class="col-md-4">
+		 <img src="images.jpg" width=100 height=100 alt="" class="img-rounded">
+		 </div>
+		 <div class="col-md-5">
+		<a href="https://twitter.com/" style="color:#202020; font-family:'typo' ; font-size:18px" title="Find on Twitter">Quiz App</a>
+		<h4 style="color:#202020; font-family:'typo' ;font-size:16px" class="title1">+977 9845063741</h4>
+		<h4 style="font-family:'typo' ">Quizapp@gmail.com</h4>
+		<h4 style="font-family:'typo' ">DeerWalk insitute of management & Technology,Ktm</h4></div></div>
+		</p>
+      </div>
+    </div>
+  </div>
+</div>
+<!--admin login-->
+	 <div class="modal fade" id="login">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title"><span style="color:orange;font-family:'typo' ">LOGIN</span></h4>
+      </div>
+      <div class="modal-body title1">
+<div class="row">
+<div class="col-md-3"></div>
+<div class="col-md-6">
+<form role="form" method="post" action="admin.php?q=index.php">
+<div class="form-group">
+  <h3 style="text-align:center;">Admin Login</h3><br>
+<input type="text" name="uname" maxlength="20"  placeholder="Admin username" class="form-control"/> 
+</div>
+<div class="form-group">
+<input type="password" name="password" maxlength="15" placeholder="Password" class="form-control"/>
+</div>
+<div class="form-group" align="center">
+<input type="submit" name="login" value="Login" class="btn btn-primary" />
+</div>
+</form>
+</div><div class="col-md-3"></div></div>
+</div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
